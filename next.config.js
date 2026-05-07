9c7904ea289112c749b0577ebdd04e11e090adff
Suman Roy <sumanrox@outlook.com>
2026-05-07 20:43:17 +0530

    added compatibility for pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
+  output: 'export',
   compiler: {
     styledComponents: true,
   },
@@ -15,6 +15,15 @@ const nextConfig = {
       buffer: 'buffer/',
       'asn1.js-rfc3280': './src/lib/asn1.js-rfc3280-patch.js',
     },
+  },
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      crypto: require.resolve('crypto-browserify'),
+      stream: require.resolve('stream-browserify'),
+      buffer: require.resolve('buffer/'),
+    };
+    return config;
   },
 };
 
